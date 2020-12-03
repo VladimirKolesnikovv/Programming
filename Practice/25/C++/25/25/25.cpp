a29ad86971a876c8a0fe1bbfc950348b0cc1dbcc
Владимир Колесников <vova_kolya02@mail.ru>
2020-12-03 14:39:55 +0400

    03.12

---
+++ Practice/25/C++/25/25/25.cpp
@@ -0,0 +1,17 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+int main()
+{
+	const int length = 4;
+	int array[length] = { 2, 1, 7, -3 };
+
+	std::sort(array, array + length);
+
+	for (int i = 0; i < length; ++i)
+		std::cout << array[i] << ' ';
+
+	return 0;
+}
