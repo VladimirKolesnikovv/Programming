081bb2aef75e9ee1c20a0718b5236ce2a4f8d975
Владимир Колесников <vova_kolya02@mail.ru>
2020-12-26 00:08:18 +0400

    29 converted

--- Practice/29/C++/29/29/29.cpp
+++ Practice/29/C++/29/29/29.cpp
@@ -1,64 +1,61 @@
-﻿#include <iostream>
-#include <string>
+﻿#include "iostream"
+#include <string.h>
+#include <stdlib.h>
+
 using namespace std;
-int main() {
-    setlocale(LC_ALL, "rus");
-    struct student {
-        string fio;
-        int group;
-        int scor[5];
-    };
-    cout << "Введите количество студентов" << endl;
-    int n;
-    cin >> n;
-    student* st = new student[n];
-    for (int i = 0; i != n; i++) {
-        cout << "Введите ФИО" << endl;
-        cin >> st[i].fio;
-        cout << "Введите номер группы" << endl;
-        cin >> st[i].group;
-        cout << "Успеваемость" << endl;
+
+#define N 10
+
+struct STUDENT
+{
+    char name[30];
+    int group;
+    int ses[5];
+};
+
+int cmp(const void* p1, const void* p2)
+{
+    return strcmp(((STUDENT*)p1)->name, ((STUDENT*)p2)->name);
+}
+
+int main()
+{
+    STUDENT stud1[N];
+
+    for (int st = 0; st < N; st++)
+    {
+        cout << "Input first name: ";
+        cin.getline(stud1[st].name, 30);
+        cout << "Input number of group: ";
+        cin >> stud1[st].group;
+
+        for (int i = 0; i < 5; i++)
+        {
+            cout << "Input mark " << i + 1 << ": ";
+            cin >> stud1[st].ses[i];
+        }
+
+        cout << endl;
+        cin.get();
+    }
+
+    qsort(stud1, N, sizeof(STUDENT), cmp);
+
+    int fl = 1;
+    for (int i = 0; i < N; i++)
+    {
         int j = 0;
-        do {
-            cout << "Оценка" << endl;
-            cin >> st[i].scor[j];
+        while ((j < 5) && (stud1[i].ses[j] != 2))
             j++;
-        } while (j != 5);
-        cout << endl;
-    }
-    cout << endl;
-    // сортируем по возрастанию групп
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n - i; i++)
-            if (st[j].group > st[j + 1].group)
-                swap(st[j].group, st[j + 1].group);
-    // выводим студентов по возрастанию групп
-    for (int i = 0; i != n; i++) {
-
-        cout << st[i].fio << endl;
-        cout << st[i].group << endl;
-        for (int j = 0; j != 5; j++) {
-            cout << st[i].scor[j] << " ";
+        if (j < 5)
+        {
+            fl = 0;
+            cout << "Student " << stud1[i].name << " (group " << stud1[i].group << ") have mark 2" << endl;
         }
-        cout << endl;
-
     }
-    cout << endl;
-    int sum = 0;
-    int ind;
-    int f = 0;
-    cout << "Студенты средний бал >4" << endl;
-    for (int i = 0; i != n; i++)
-        if (((st[i].scor[0] + st[i].scor[1] + st[i].scor[2] + st[i].scor[3] + st[i].scor[4]) / 5) > 4) {
-            f++;
-            ind = i;
-        }
-    if (f > 0) {
-        cout << st[ind].fio << endl;
-        cout << st[ind].group << endl;
-    }
-    else cout << "Таких нет" << endl;
+    if (fl)
+        cout << "No students, which have mark 2" << endl;
 
-    system("pause");
+    cin.get();
     return 0;
 }
